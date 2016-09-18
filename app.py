d680bcc1327fba870571bcd9e76e0dd394726c5d
Domi <dimbs09@gmail.com>
2016-09-18 19:58:59 +0900

    ADDED: 1) isaax.json

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 
 import urllib
 
-url='http://127.0.0.1:48080' # isaaxd server
+url='http://127.0.0.1:48080/v1/ping' # isaaxd server ping request
 req=urllib.urlopen(url)
-print("Hello Isaax!", req.getcode())
+print("Hello, Isaax! status code:", req.getcode())
+
+print("Bye!")
